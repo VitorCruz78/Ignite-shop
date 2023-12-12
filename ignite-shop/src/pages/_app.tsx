7184e4df98ad842ccf7be4c2bfcdbcd76e926a17
VitorCruz78 <vitorluiz15012006@gmail.com>
2023-12-11 22:02:30 -0300

    Criação header e estrutura da home

--- ignite-shop/src/pages/_app.tsx
+++ ignite-shop/src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { globalStyles } from "@/styles/global"
 import { AppProps } from "next/app"
 
+import LogoImg from '../assets/Logo.png'
+import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
+
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <Container>
+            <Header>
+                <Image src={LogoImg} width={120} height={40} alt="Logo" />
+            </Header>
+            <Component {...pageProps} />
+        </Container>
+    )
 }
